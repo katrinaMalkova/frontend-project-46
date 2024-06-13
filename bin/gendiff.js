04dd69f038c15f16e8d3d5dede5e7f8bea981437
katrinaMalkova <kitty.malkova@gmail.com>
2024-06-13 10:12:07 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,5 +9,6 @@ program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
     .option('-f, --format [type]' , 'output format' )
+    .arguments('<filepath1 , filepath2');
 
 program.parse();
