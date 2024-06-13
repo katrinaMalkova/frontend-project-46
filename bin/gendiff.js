32a25a858cda494de5bf41ca6469babad3a5905a
katrinaMalkova <kitty.malkova@gmail.com>
2024-06-13 10:03:50 +0300

    delete

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,7 @@ const program = new Command();
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+    .version('1.0.0')
+    .option('-f, --format [type]' , 'output format' )
 
 program.parse();
